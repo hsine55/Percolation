9664b09b1a9f2527c65e3802eb905ba179abf3c1
hsine55 <hsine.benmessaoud@gmail.com>
2017-02-27 21:08:49 +0100

    fixed backsplash problem

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,106 +1,112 @@
+/*
+ * Fiducia IT AG, All rights reserved. Use is subject to license terms.
+ */
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	
-	private int[][] systemGrid; //systemGrid[i] = 0 : site is blocked // systemGrid[i] = 1 : site is open
-	private int openCount = 0;
-	private int gridDimension = 0;
-	private WeightedQuickUnionUF weightedQuickUnionUF;
-	private int top;
-	private int bottom;
-	
-	public Percolation(int n) {
-		if (n <= 0) throw new java.lang.IndexOutOfBoundsException();
-		gridDimension = n;
-		systemGrid = new int[n][n];
-		bottom = n*n;
-		top = n*n + 1;
-		weightedQuickUnionUF = new WeightedQuickUnionUF(gridDimension*gridDimension + 2);
-		for (int i=0 ; i<n ; i++) {
-			for (int j=0 ; j<n ; j++) {
-				systemGrid[i][j] = 0;
-			}
-		}
-	}
-	
-	public void open(int row, int col) {
-		if (!isValid(row, col)) {
-			throw new java.lang.IndexOutOfBoundsException();
-		}
-		systemGrid[row-1][col-1] = 1;
-		openCount++;
-		connectOpenNeighbours(row , col);
-	}
-	
-	private void connectOpenNeighbours(int row, int col) {
-		int leftNeighbour = col - 1;
-		int rightNeighbour = col + 1;
-		int upperNeighbour = row - 1;
-		int downNeighbour = row + 1;
-		
-		if (isValid(row, leftNeighbour) && isOpen(row, leftNeighbour)) {
-			weightedQuickUnionUF.union(((row-1)*gridDimension)+col-1, ((row-1)*gridDimension)+leftNeighbour-1);
-			if (row == 1 ) { weightedQuickUnionUF.union(top, ((row-1)*gridDimension)+leftNeighbour-1);};
-			if (row == gridDimension) { weightedQuickUnionUF.union(bottom, ((row-1)*gridDimension)+leftNeighbour-1);};
+    
+    private int[][] systemGrid; //systemGrid[i] = 0 : site is blocked // systemGrid[i] = 1 : site is open
+    private int openCount = 0;
+    private int gridDimension = 0;
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private	WeightedQuickUnionUF weightedQuickUnionUFBackSlash; //needed to keep track of the actually full nodes.
+    private int fullTop;													//So that we don't get full nodes that aren't connected to the top.
+    private int top;
+    private int bottom;
+    
+    public Percolation(int n) {
+        if (n <= 0) throw new java.lang.IllegalArgumentException();
+        gridDimension = n;
+        systemGrid = new int[n][n];
+        bottom = n*n;
+        top = n*n + 1;
+        weightedQuickUnionUF = new WeightedQuickUnionUF(gridDimension*gridDimension + 2);
+        fullTop = n*n;
+        weightedQuickUnionUFBackSlash = new WeightedQuickUnionUF(gridDimension*gridDimension + 1);
+    }
+    
+    public void open(int row, int col) {
+        if (!isValid(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if (isOpen(row, col)) return; //already open
+        systemGrid[row-1][col-1] = 1;
+        openCount++;
+        if(row == 1) {
+            weightedQuickUnionUF.union(top, getWUFIndex(row, col));
+            weightedQuickUnionUFBackSlash.union(fullTop, getWUFIndex(row, col));
+        } 
+        if (row == gridDimension) {
+            weightedQuickUnionUF.union(bottom, getWUFIndex(row, col));
+        }
+        connectOpenNeighbours(row , col);
+    }
+    
+    private void connectOpenNeighbours(int row, int col) {
+        int leftNeighbour = col - 1;
+        int rightNeighbour = col + 1;
+        int upperNeighbour = row - 1;
+        int downNeighbour = row + 1;
+        
+        if (isValid(row, leftNeighbour) && isOpen(row, leftNeighbour)) {
+            weightedQuickUnionUF.union(getWUFIndex(row, col), getWUFIndex(row, leftNeighbour));
+            weightedQuickUnionUFBackSlash.union(getWUFIndex(row, col), getWUFIndex(row, leftNeighbour));
+        }
+        
+        if (isValid(row, rightNeighbour) && isOpen(row, rightNeighbour)) {
+            weightedQuickUnionUF.union(getWUFIndex(row, col), getWUFIndex(row, rightNeighbour));
+            weightedQuickUnionUFBackSlash.union(getWUFIndex(row, col), getWUFIndex(row, rightNeighbour));
+        }
+        
+        if (isValid(upperNeighbour, col) && isOpen(upperNeighbour, col)) {
+            weightedQuickUnionUF.union(getWUFIndex(row, col), getWUFIndex(upperNeighbour, col));
+            weightedQuickUnionUFBackSlash.union(getWUFIndex(row, col), getWUFIndex(upperNeighbour, col));
+        }
+        
+        if (isValid(downNeighbour, col) && isOpen(downNeighbour, col)) {
+            weightedQuickUnionUF.union(getWUFIndex(row, col), getWUFIndex(downNeighbour, col));
+            weightedQuickUnionUFBackSlash.union(getWUFIndex(row, col), getWUFIndex(downNeighbour, col));
+        }
+        
+        
+    }
+    
+    private boolean isValid(int row, int col) {
+        if ((row <= 0 || row > gridDimension) || (col <= 0 || col > gridDimension)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean isOpen(int row, int col) {
+        if (!isValid(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return systemGrid[row-1][col-1] == 1;
+    }
+    
+    public boolean isFull(int row, int col) {
+        if (!isValid(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return weightedQuickUnionUFBackSlash.connected(fullTop, getWUFIndex(row, col));
+    }
+    
+    public int numberOfOpenSites() {
+        return openCount;
+    }
+    
+    public boolean percolates() {
+        return weightedQuickUnionUF.connected(top, bottom);
+    }
+    
+    private int getWUFIndex(int gridIndX, int gridIndY) {
+        return (gridIndX-1)*gridDimension + gridIndY - 1;
+    }
+    
+    public static void main(String[] args) {
 
-		}
-		
-		if (isValid(row, rightNeighbour) && isOpen(row, rightNeighbour)) {
-			weightedQuickUnionUF.union(((row-1)*gridDimension)+col-1, ((row-1)*gridDimension)+rightNeighbour-1);
-			if (row == 1) { weightedQuickUnionUF.union(top, ((row-1)*gridDimension)+rightNeighbour-1);};
-			if (row == gridDimension) { weightedQuickUnionUF.union(bottom, ((row-1)*gridDimension)+rightNeighbour-1);};
-		}
-		
-		if (isValid(upperNeighbour, col) && isOpen(upperNeighbour, col)) {
-			weightedQuickUnionUF.union(((row-1)*gridDimension)+col-1, ((upperNeighbour-1)*gridDimension));
-			if (row == 1) { weightedQuickUnionUF.union(top, (upperNeighbour-1)*gridDimension);}
-			if (row == gridDimension) { weightedQuickUnionUF.union(bottom, (upperNeighbour-1)*gridDimension);};
-
-		}
-		
-		if (isValid(downNeighbour, col) && isOpen(downNeighbour, col)) {
-			weightedQuickUnionUF.union(((row-1)*gridDimension)+col-1, ((downNeighbour-1)*gridDimension));
-			if (row == 1) { weightedQuickUnionUF.union(top, (downNeighbour-1)*gridDimension);}
-			if (row == gridDimension) { weightedQuickUnionUF.union(bottom, (downNeighbour-1)*gridDimension);};
-		}
-		
-		
-	}
-	
-	private boolean isValid(int row, int col) {
-		if ((row <= 0 || row > gridDimension) || (col <= 0 || col > gridDimension)) {
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean isOpen(int row, int col) {
-		if (!isValid(row, col)) {
-			throw new java.lang.IndexOutOfBoundsException();
-		}
-		return systemGrid[row-1][col-1] == 1;
-	}
-	
-	public boolean isFull(int row, int col) {
-		if (!isValid(row, col)) {
-			throw new java.lang.IndexOutOfBoundsException();
-		}
-		return systemGrid[row-1][col-1] == 0;
-	}
-	
-	public int numberOfOpenSites() {
-		return openCount;
-	}
-	
-	public boolean percolates() {
-		return weightedQuickUnionUF.connected(top, bottom);
-	}
-	
-	public static void main(String[] args) {
-		Percolation test = new Percolation(3);
-		int[][] fuck = new int[3][3];
-		fuck[0][1] = 1;
-		fuck[0][2] = 1;
-	}
-	
+    }
+    
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -3,43 +3,43 @@ import edu.princeton.cs.algs4.StdStats;
 
 
 public class PercolationStats {
-	private double mean;
-	private double standardDeviation;
-	private int trials;
-	/**
-	 * perform trials independent experiments on an n-by-n grid
-	 * @param n
-	 * @param trials
-	 */
+    private double mean;
+    private double standardDeviation;
+    private int trials;
+    /**
+     * perform trials independent experiments on an n-by-n grid
+     * @param n
+     * @param trials
+     */
    public PercolationStats(int n, int trials) {
-	   if (n<=0 || trials<=0) {
-		   throw new java.lang.IndexOutOfBoundsException();
-	   }
-	   this.trials = trials;
-	   double[] fractions = new double[trials];
-	   for (int i=1; i<=trials; i++) {
-		   System.out.println("trial number : " + i);
-		   double currentFraction = runPercolation(n);
-		   fractions[i-1] = currentFraction;
-	   }
-	   this.standardDeviation = StdStats.stddev(fractions);
-	   this.mean = StdStats.mean(fractions);
-	    
+       if (n<=0 || trials<=0) {
+           throw new java.lang.IndexOutOfBoundsException();
+       }
+       this.trials = trials;
+       double[] fractions = new double[trials];
+       for (int i=1; i<=trials; i++) {
+           System.out.println("trial number : " + i);
+           double currentFraction = runPercolation(n);
+           fractions[i-1] = currentFraction;
+       }
+       this.standardDeviation = StdStats.stddev(fractions);
+       this.mean = StdStats.mean(fractions);
+        
    }
    
    private double runPercolation(int n) {
-	   Percolation percolationSystem = new Percolation(n);
-	   while(!percolationSystem.percolates()) {
-		   int siteX = StdRandom.uniform(1, n+1);
-		   int siteY = StdRandom.uniform(1, n+1);
-		   while(percolationSystem.isOpen(siteX, siteY)) {
-			   siteX = StdRandom.uniform(1, n+1);
-			   siteY = StdRandom.uniform(1, n+1);
-		   }
-		   percolationSystem.open(siteX, siteY);
-	   }
-	   double fractionOfOpenSites = (double)percolationSystem.numberOfOpenSites();
-	   return fractionOfOpenSites / (n*n);
+       Percolation percolationSystem = new Percolation(n);
+       while(!percolationSystem.percolates()) {
+           int siteX = StdRandom.uniform(1, n+1);
+           int siteY = StdRandom.uniform(1, n+1);
+           while(percolationSystem.isOpen(siteX, siteY)) {
+               siteX = StdRandom.uniform(1, n+1);
+               siteY = StdRandom.uniform(1, n+1);
+           }
+           percolationSystem.open(siteX, siteY);
+       }
+       double fractionOfOpenSites = (double)percolationSystem.numberOfOpenSites();
+       return fractionOfOpenSites / (n*n);
    }
    
    /**
@@ -47,7 +47,7 @@ public class PercolationStats {
     * @return
     */
    public double mean() {
-	  return mean; 
+      return mean; 
    }
    
    /**
@@ -55,7 +55,7 @@ public class PercolationStats {
     * @return
     */
    public double stddev() {
-	  return standardDeviation; 
+      return standardDeviation; 
    }
    
    /**
@@ -63,7 +63,7 @@ public class PercolationStats {
     * @return
     */
    public double confidenceLo() {
-	   return (mean - ( (Math.sqrt(standardDeviation)*1.96) / Math.sqrt(trials)));
+       return (mean - ( (Math.sqrt(standardDeviation)*1.96) / Math.sqrt(trials)));
    }
    
    /**
@@ -71,14 +71,14 @@ public class PercolationStats {
     * @return
     */
    public double confidenceHi() {
-	   return (mean + ( (Math.sqrt(standardDeviation)*1.96) / Math.sqrt(trials)));
+       return (mean + ( (Math.sqrt(standardDeviation)*1.96) / Math.sqrt(trials)));
    }
 
 
    public static void main(String[] args) {
-	   PercolationStats test = new PercolationStats(15, 1000);
-	   System.out.println("mean = " + test.mean);
-	   System.out.println("standard deviation = " + test.standardDeviation);
-	   System.out.println("95% confidence interval = [" + test.confidenceLo() + ", " + test.confidenceHi() + "]");
+       PercolationStats test = new PercolationStats(1, 100000);
+       System.out.println("mean = " + test.mean);
+       System.out.println("standard deviation = " + test.standardDeviation);
+       System.out.println("95% confidence interval = [" + test.confidenceLo() + ", " + test.confidenceHi() + "]");
    }
 }
